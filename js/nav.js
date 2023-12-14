8983df202ac58495db4ca8738cf850eacbf02dac
McKenzie Addison <146353691+heinskeys@users.noreply.github.com>
2023-12-14 13:03:18 -0500

    deployment + fonts

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,4 @@
 function myFunction() {
-    var x = document.getElementById("navlinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+  var x = document.getElementById("navlinks");
+  x.classList.toggle("show");
+}
