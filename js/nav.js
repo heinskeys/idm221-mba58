77991bb797076d06429206306ea7c38a644844aa
McKenzie Addison <146353691+heinskeys@users.noreply.github.com>
2023-12-15 07:02:11 -0500

    removed js

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navLinks = document.getElementById('navlinks');
-
-  menuToggle.addEventListener('click', function() {
-      navLinks.classList.toggle('show');
-  });
-});
